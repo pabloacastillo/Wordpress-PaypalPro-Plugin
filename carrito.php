1fc500c889afa370bf867521f69616f5bf1b8549
pabloacastillo <pabloacastillo@gmail.com>
2010-06-21 15:50:55 -0400

    Added option to configure the image for the boy button. Added a new folder with images for the buy button. Created a new file for the content of the Payshop Pro admin con wp-admin (paypal_wpadmin.php)

--- carrito.php
+++ carrito.php
@@ -42,15 +42,31 @@ function carrito_conf() {
         // PAYPAL PRO SIGNATURE
 		delete_option('carrito_paypal_signature');
 		add_option('carrito_paypal_signature', $_POST['signature_paypal']);
+
         // PAYPAL PRO USERNAME
 		delete_option('carrito_paypal_username');
 		add_option('carrito_paypal_username', $_POST['username_paypal']);
+
         // PAYPAL PRO PASSWORD
 		delete_option('carrito_paypal_password');
 		add_option('carrito_paypal_password', $_POST['password_paypal']);
+
         // ACTIVATE OR DEACTIVATE PAYPAL SANDBOX FOR TESTING
 		delete_option('carrito_paypal_sandbox');
 		add_option('carrito_paypal_sandbox', $_POST['sandbox_paypal']);
+
+
+        // SELECT THE CURRENCY YOU WOULD BE USING
+		delete_option('carrito_paypal_currency');
+		add_option('carrito_paypal_currency', $_POST['currency_paypal']);
+
+        // SELECT THE IMAGE FOR THE BUY BUTTON YOU WOULD BE USING
+		delete_option('carrito_paypal_button');
+		add_option('carrito_paypal_button', $_POST['button_paypal']);
+		if($_POST['carrito_paypal_button_url']){
+            delete_option('carrito_paypal_button_url');
+            add_option('carrito_paypal_button_url', $_POST['carrito_paypal_button_url']);
+		}
 	}
 
 
@@ -100,96 +116,13 @@ function carrito_conf() {
 	}
 
 	$messages = array();
-?>
-<?php if ( !empty($_POST ) ) : ?>
-<div id="message" class="updated fade"><p><strong><?php _e('Options saved.') ?></strong></p></div>
-<?php endif; ?>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<style type="text/css">
-label{ display:block; width: 150px; float:left; }
-</style>
-<div class="wrap">
-	<h2><?php _e('Paypal API Information'); ?></h2>
-	<div class="narrow">
-		<form action="" method="post" style="">
-			<label for="username_paypal">Paypal Username:</label> <input type="text" name="username_paypal" value="<?php echo get_option('carrito_paypal_username');?>">
-			<br />
-			<label for="password_paypal">Paypal Password:</label> <input type="text" name="password_paypal" value="<?php echo get_option('carrito_paypal_password');?>">
-			<br />
-			<label for="signature_paypal">Paypal Signature:</label> <input type="text" name="signature_paypal" value="<?php echo get_option('carrito_paypal_signature');?>">
-			<br />
-			<label for="sandbox_paypal">Sandbox Mode:</label> <input type="checkbox" name="sandbox_paypal" <?php if(get_option('carrito_paypal_sandbox')=='active'){ echo 'checked'; }?> value="active">
-			<br />
-			<input type="submit" name="carrito_config" value="Save Configuration">
-		</form>
+    ?>
+    <?php if ( !empty($_POST ) ) : ?>
+        <div id="message" class="updated fade"><p><strong><?php _e('Options saved.') ?></strong></p></div>
+    <?php endif;
 
-	</div>
-	<h2><?php _e('Flat Shipping options'); ?></h2>
-	<?php $activate_flat_shipping_paypal=get_option('activate_flat_shipping_paypal'); ?>
-	<div class="narrow">
-    	<form action="" method="post" style="">
-            Activate Flat Shipping: <input type="checkbox" name="activate_flat_shipping_paypal" <?php if($activate_flat_shipping_paypal=='active'){ echo 'checked'; }?> value="active">
-            <input type="submit" name="save_flat_shipping_paypal" value="Save Shipping Options">
-        </form>
-    	<?php if($activate_flat_shipping_paypal=='active') { ?>
-            <h3>Add New</h3>
-            <form action="" method="post" style="">
-            	<label for="carrito_shipping_name">Shipping Name:</label> <input name="carrito_shipping_name" value="">
-            	<br />
-            	<label for="carrito_shipping_price">Shipping Price:</label> <input name="carrito_shipping_price" value="">
-            	<br />
-            	<input type="submit" name="carrito_shipping_new" value="Add New Shipping Option">
-            </form>
+    include_once 'paypal_wpadmin.php';
 
-            <form action="" method="post" style="padding:25px 0px 0px 25px">
-            	<?php
-            	$sopt=explode('|',get_option('carrito_shipping'));
-            	for($i=1; $i<(count($sopt));$i++){
-            	?>
-            		<div>
-            			<input type="button" onclick="$(this).parent().html('');" value="Remove">
-            			<input name="carrito_shipping_names[]" value="<?php echo $sopt[$i];?>">
-            			<?php $i++;?>
-            			<input name="carrito_shipping_prices[]" value="<?php echo $sopt[$i];?>">
-            		</div>
-            	<?php
-            	}
-            	?>
-            	<?php if(count($sopt)>1) { ?><input type="submit" name="carrito_shippings" value="Update Shipping Options"> <?php } ?>
-            </form>
-        <?php } ?>
-	</div>
-
-	<h2><?php _e('Coupon codes'); ?></h2>
-	<div class="narrow">
-		<h3>Add New</h3>
-		<form action="" method="post" style="">
-			<label for="carrito_discount_name">Code:</label> <input name="carrito_discount_name" value="">(ex: ABC1234)
-			<br />
-			<label for="carrito_discount_price">Discount:</label> <input name="carrito_discount_price" value="">(ex: 10% or 15$)
-			<br />
-			<input type="submit" name="carrito_discount_new" value="Add New Coupon Code">
-		</form>
-
-		<form action="" method="post" style="padding:25px 0px 0px 25px">
-		<?php
-		$copt=explode('|',get_option('carrito_discounts'));
-		for($i=1; $i<(count($copt));$i++){
-			?>
-			<div>
-			<input type="button" onclick="$(this).parent().html('');" value="Remove">
-			<input name="carrito_discount_names[]" value="<?php echo $copt[$i];?>">
-			<?php $i++;?>
-			<input name="carrito_discount_prices[]" value="<?php echo $copt[$i];?>">
-			</div>
-			<?php
-		}
-		?>
-		<?php if(count($copt)>1) { ?><input type="submit" name="carrito_discounts" values="Update Coupon Codes"><?php } ?>
-		</form>
-	</div>
-</div>
-<?php
 }
 
 
